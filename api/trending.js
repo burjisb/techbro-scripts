0aa8b63983451c02057e7ce957b4608fbeb7a8a7
Burjis <billimoriaburjis@gmail.com>
2026-03-12 14:08:06 +0530

    fix env key

--- api/trending.js
+++ api/trending.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-    const key = process.env.VITE_GEMINI_KEY;
+    const key = process.env.GEMINI_KEY;
     
     const response = await fetch(
       `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${key}`,
